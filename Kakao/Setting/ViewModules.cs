dd208d752f2bdae70e7f617997b03ae4a840902a
ssamae <ssamae87@gmail.com>
2025-10-05 00:14:17 +0900

    123Kakao.Login 추가

---
+++ Kakao/Setting/ViewModules.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakao.Setting
+{
+    internal class ViewModules : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<ViewModules>();
+        }
+    }
+}
