7b34865d6747c5fd9ebdb9170b72f36324ede2b1
ssamae <ssamae87@gmail.com>
2025-10-05 02:15:40 +0900

    updates _ 2:39

--- Kakao.Login/Local/ViewModels/LoginContentViewModel.cs
+++ Kakao.Login/Local/ViewModels/LoginContentViewModel.cs
@@ -1,24 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Controls;
 using Jamesnet.Wpf.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Prism.Ioc;
+using Prism.Regions;
 
 namespace Kakao.Login.Local.ViewModels
 {
     public partial class LoginContentViewModel : ObservableBase
     {
-        public LoginContentViewModel()
+        private readonly IRegionManager _regionManager;
+        private readonly IContainerProvider _containerProvider;
+
+        public LoginContentViewModel(IRegionManager regionManager, IContainerProvider containerProvider)
         {
-            
+            _regionManager = regionManager;
+            _containerProvider = containerProvider;
         }
 
 
         [RelayCommand]
         private void Login()
-        { 
+        {
+            IRegion mainRegion = _regionManager.Regions["MainRegion"];
+            IViewable friendsContent = _containerProvider.Resolve<IViewable>("FriendsContent");
+
+            if(!mainRegion.Views.Contains(friendsContent))
+            {
+                mainRegion.Add(friendsContent);
+            }
+
+            mainRegion.Activate(friendsContent);
         }
     }
 }
