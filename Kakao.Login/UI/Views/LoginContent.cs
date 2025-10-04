43faea18f53401e9d31649d207b1ccd0a310dc39
ssamae <ssamae87@gmail.com>
2025-10-04 23:59:19 +0900

    Kakao.Login 추가

---
+++ Kakao.Login/UI/Views/LoginContent.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Kakao.Login.UI.Views
+{
+    public class LoginContent : Window
+    {
+        static LoginContent()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LoginContent), new FrameworkPropertyMetadata(typeof(LoginContent)));
+        }
+    }
+}
