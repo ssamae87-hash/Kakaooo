ea4620e158d28f48d9174645ebb24c8b44cc8729
ssamae <ssamae87@gmail.com>
2025-10-07 17:03:09 +0900

    KakaoWindow 및 LoginContent 스타일 수정 및 프로젝트 추가
    KakaoWindow.xaml에 Opacity와 SizeToContent 속성을 추가하여 창의 투명도와 크기 조정 방식을 설정했습니다. KakaoWindow.cs에 기본 생성자를 추가하고, LoginContent.xaml에서 Background 색상을 변경하고 크기(Width, Height) 속성을 추가했습니다. 또한, KakaoCI 로고를 표시하는 Viewbox와 Path 요소를 추가했습니다.
    
    마지막으로, Kakao.sln에 Foundation 프로젝트를 새로 추가했습니다.

--- Kakao.Forms/UI/Views/KakaoWindow.cs
+++ Kakao.Forms/UI/Views/KakaoWindow.cs
@@ -10,5 +10,11 @@ namespace Kakao.Forms.UI.Views
             DefaultStyleKeyProperty.OverrideMetadata(typeof(KakaoWindow),
                 new FrameworkPropertyMetadata(typeof(KakaoWindow)));
         }
+
+        public KakaoWindow()
+        {
+            //WindowStyle = WindowStyle.None;
+            //AllowsTransparency = true;
+        }
     }
 }
