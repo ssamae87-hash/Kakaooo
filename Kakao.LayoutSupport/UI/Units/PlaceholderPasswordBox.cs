3c3e8ee24d1e67902823a30f08aa40c9ae294431
ssamae <ssamae87@gmail.com>
2025-10-08 14:36:05 +0900

    Add PlaceholderPasswordBox control and integrate it
    새로운 PlaceholderPasswordBox 컨트롤을 추가하고 관련 파일에 통합하였습니다.
    
    - `Generic.xaml`에 PlaceholderPasswordBox 스타일 리소스 추가
    - `PlaceholderTextBox.cs`에서 불필요한 using 구문 제거 및 코드 스타일 수정
    - `LoginContent.xaml`에서 PlaceholderTextBox를 PlaceholderPasswordBox로 대체
    - `PlaceholderPasswordBox.xaml` 파일 추가: 스타일 및 템플릿 정의
    - `PlaceholderPasswordBox.cs` 파일 추가: 사용자 정의 컨트롤 구현

---
+++ Kakao.LayoutSupport/UI/Units/PlaceholderPasswordBox.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kakao.LayoutSupport.UI.Units
+{
+    public class PlaceholderPasswordBox : TextBox
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(nameof(PlaceholderText), typeof(string), typeof(PlaceholderPasswordBox), new PropertyMetadata(""));
+
+        static PlaceholderPasswordBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderPasswordBox), new FrameworkPropertyMetadata(typeof(PlaceholderPasswordBox)));
+        }
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_PasswordBox") is PasswordBox pwd)
+            {
+                pwd.PasswordChanged += Pwd_PasswordChanged;
+            }
+        }
+
+        private void Pwd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox pwd)
+            {
+                SetValue(TextProperty, pwd.Password);
+            }
+        }
+
+        private void CustomPasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == this)
+            {
+                PasswordBox passwordBox = GetTemplateChild("PART_PasswordBox") as PasswordBox;
+                if (passwordBox != null)
+                {
+                    passwordBox.Focus();
+                }
+            }
+        }
+    }
+}
--- Kakao.LayoutSupport/UI/Units/PlaceholderTextBox.cs
+++ Kakao.LayoutSupport/UI/Units/PlaceholderTextBox.cs
@@ -1,33 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kakao.LayoutSupport.UI.Units
 {
-
     public class PlaceholderTextBox : TextBox
     {
-        public static readonly DependencyProperty PlaceholderTextProperty= DependencyProperty
-            .Register(nameof(PlaceholderText),
-            typeof(string),
-            typeof(PlaceholderTextBox),
-            new PropertyMetadata(""));
+        public static readonly DependencyProperty PlaceholderTextProperty
+            = DependencyProperty.Register(nameof(PlaceholderText),
+                                            typeof(string),
+                                            typeof(PlaceholderTextBox),
+                                            new PropertyMetadata(""));
 
         static PlaceholderTextBox()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderTextBox),
-                new FrameworkPropertyMetadata(typeof(PlaceholderTextBox)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderTextBox), new FrameworkPropertyMetadata(typeof(PlaceholderTextBox)));
         }
 
         public string PlaceholderText
